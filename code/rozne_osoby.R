9b6d29c363f9d625bb680a32ea5f785649bbfbcd
Janekkr <72567101+Janekkr@users.noreply.github.com>
2021-12-23 20:28:53 +0100

    rozne osoby

---
+++ code/rozne_osoby.R
@@ -0,0 +1,18 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+source("wczytanie_json.R")
+
+
+df <- load_all_conversations()
+
+rozne_osoby <- function(df){
+  df1 <- df %>% group_by(Date_Y_M_D) %>% summarise(il_osob = n_distinct(sender_name))
+  df1
+}
+
+#1. opcja na przestrzeni lat ile osob danego dnia
+#ggplot(rozne_osoby(df), aes(Date_Y_M_D, il_osob)) +
+#  geom_line()
+
+#2. topka dni
+#ggplot(rozne_osoby(df) %>% arrange(-il_osob) %>%  head(10), aes(reorder(as.character(Date_Y_M_D), -il_osob), il_osob))+
+#  geom_col() 
